bac5aed09a5326816f40a4a8606128de917a20e9
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2026-01-08 13:27:37 -0800

    added sql queries to read and manipulate data

--- query.sql
+++ query.sql
@@ -1,71 +1,220 @@
-import { PGlite } from '@electric-sql/pglite';
-import fs from 'fs';
+          -- SELECTING COLUMNS --
+-- SELECT * FROM cars;
+-- SELECT brand, model, price FROM cars;
 
-(async () => {
-  const db = new PGlite();
-  await db.exec(`
-        CREATE TABLE IF NOT EXISTS cars (
-            id SERIAL PRIMARY KEY,
-            brand TEXT,
-            model TEXT,
-            year INTEGER,
-            price INTEGER,
-            color TEXT,
-            condition INTEGER,
-            sold BOOLEAN
-        );
-        INSERT INTO cars (brand, model, year, price, color, condition, sold
-        ) VALUES 
-          ('Ford', 'Mustang', 1965, 45000, 'white', 4, false),
-          ('Chevrolet', 'Camaro', 1970, 48000, 'red', 2, false),
-          ('Dodge', 'Charger', 1969, 58000, 'black', 4, true),
-          ('Porsche', '911', 1985, 85000, 'silver', 5, false),
-          ('Jaguar', 'E-Type', 1967, 56000, 'green', 2, true),
-          ('Jaguar', 'S-Type', 1963, 100000, 'dark green', 3, true),
-          ('Jaguar', 'X-Type', 2001, 10000, 'black', 3, true),
-          ('BMW', 'M3', 1990, 35000, 'green-yellow', 1, true),
-          ('Ferrari', 'F355', 1997, 150000, 'red', 5, false),
-          ('Ford', 'Mustang', 1967, 15000, 'dark blue', 0, false),
-          ('Aston Martin', 'DB5', 1964, 595000, 'silver', 5, false),
-          ('Aston Martin', 'DB4', 1960, 465000, 'light green', 5, false),
-          ('Aston Martin', 'DBS', 1969, 99000, 'red', 2, false),
-          ('Aston Martin', 'DB4', 1960, 425000, 'green', 3, false),
-          ('Aston Martin', 'DB5', 1965, 649000, 'dark red', 5, false),
-          ('Toyota', 'Supra', 1994, 68000, 'black', 4, true),
-          ('Nissan', 'Skyline GT-R', 1999, 95000, 'blue', 5, false),
-          ('Volkswagen', 'Beetle', 1963, 25000, 'yellow', 3, true),
-          ('Lamborghini', 'Countach', 1989, 320000, 'red', 5, false),
-          ('Rolls-Royce', 'Silver Shadow', 1975, 55000, 'white', 2, true),
-          ('Bentley', 'Continental GT', 2005, 85000, 'black', 5, false),
-          ('Maserati', 'GranTurismo', 2010, 75000, 'blue', 4, true),
-          ('Alfa Romeo', 'Spider', 1986, 28000, 'red', 3, true),
-          ('Ford', 'Mustang', 1965, 20000, 'dark red', 1, true),
-          ('Lotus', 'Esprit', 1993, 62000, 'light yellow', 4, false),
-          ('Triumph', 'Herald', 1965, 12500, 'cream', 3, true),
-          ('Ford', 'Capri', 1983, 22000, 'blue', 2, false),
-          ('Ford', 'Granada', 1977, 18000, 'black', 1, false),
-          ('Volkswagen', 'Golf GTI', 1991, 12500, 'light green', 1, true),
-          ('Chevrolet', 'Camaro', 1969, 54000, 'mint green', 5, true),
-          ('Chevrolet', 'Corvette', 1967, 88000, 'red', 5, true),
-          ('Chevrolet', 'Corvette C5', 2001, 32000, 'yellow', 4, true),
-          ('Ferrari', 'Testarossa', 1988, 195000, 'red', 5, true),
-          ('Ferrari', '360 Modena', 2003, 125000, 'silver', 5, true),
-          ('Bentley', 'Arnage', 2001, 45000, 'black', 4, false),
-          ('Bentley', 'Continental R', 1999, 68000, 'blue', 5, false),
-          ('Jaguar', 'XJ220', 1994, 450000, 'silver', 5, false),
-          ('Porsche', '911 Carrera', 1985, 85000, 'red', 5, false),
-          ('Porsche', '911 Turbo', 1995, 12000, 'black', 1, false),
-          ('Porsche', '944 Turbo', 1986, 48000, 'white', 4, true),
-          ('Porsche', '356B', 1960, 265000, 'silver', 4, false),
-          ('Bentley', 'T2', 1978, 52000, 'silver', 4, false);
-`);
 
-  // Load the SQL file
-  const query = fs.readFileSync('query.sql', 'utf8');
+          -- ADDING CONDITIONS --
+-- SELECT brand, model, price FROM cars
+--   WHERE color = 'black';
 
-  // Executing simple queries for sections 1 - 3
-  const response = await db.query(query);
+-- SELECT brand, model, condition, price FROM cars 
+--   WHERE condition = '0';
 
-  console.clear();
-  console.table(response.rows);
-})();
+
+          -- NUMERICAL FILTERING --
+-- SELECT brand, condition, price FROM cars
+--   WHERE condition > 3;
+
+-- SELECT brand, model, condition, price FROM cars 
+--   WHERE price < 50000
+
+-- SELECT brand, model, price, year FROM cars 
+--   WHERE year != 1965;
+
+-- SELECT brand, model, price, color FROM cars
+--   WHERE color <> 'yellow';
+
+-- SELECT brand, model, color, year FROM cars
+--   WHERE model LIKE 'DB_';
+
+
+          -- COMBINING CONDITIONS --
+-- SELECT brand, model, color, year FROM cars
+--   WHERE color NOT LIKE '%green%'
+--   AND model LIKE 'DB_'
+--   AND year > 1964;
+
+-- SELECT brand, model, year, condition, price FROM cars
+--   WHERE condition >= 3
+--   AND year < 1970
+--   AND price < 100000;
+
+-- SELECT brand, model, year, price FROM cars
+--   WHERE year > 1979 AND year < 1990;
+
+-- SELECT brand, model, year, price FROM cars
+--   WHERE year BETWEEN 1980 AND 1989;
+
+-- SELECT brand, model, color, price, condition FROM cars
+--   WHERE price BETWEEN 20000 AND 60000
+--   AND condition BETWEEN 1 AND 3
+--   AND color LIKE '%red%';
+
+
+          -- COMBINE BUT SELECT CONDITIONS --
+-- SELECT brand, model, condition, price FROM cars
+--   WHERE price < 250000
+--   OR brand = 'Porsche';
+
+-- SELECT brand, model, condition, price FROM cars
+--   WHERE (price < 250000
+--   OR brand = 'Porsche')
+--   AND condition > 3;
+
+-- SELECT brand, model, year, color, price, FROM cars
+--   WHERE color LIKE '%red%' OR year BETWEEN 1960 AND 1969;
+
+-- SELECT brand, model, year, color, price, sold FROM cars
+--   WHERE (color LIKE '%red%' OR year BETWEEN 1960 AND 1969)
+--   AND sold IS FALSE;
+
+
+        -- SEARCHING FOR MULTIPLE VALUES IN A COLUMN --
+-- SELECT brand, model, price, sold FROM cars
+--   WHERE brand in ('Ford', 'Chevrolet', 'Ferrari')
+--   AND sold IS FALSE;
+
+-- SELECT brand, model, condition, year FROM cars
+--   WHERE year IN (1961, 1963, 1965, 1967, 1969)
+--   AND condition >= 3 
+--   AND sold IS FALSE;
+
+-- SELECT brand, model, price, sold FROM cars
+--   WHERE (brand NOT IN ('Ford', 'Triumph', 'Chevrolet', 'Dodge')
+--   OR price < 50000)
+--   AND sold IS FALSE;
+
+
+          -- CHALLENGES --
+-- SELECT brand, model, color from cars
+--   WHERE color LIKE '%red%'
+--   AND brand NOT IN ('Ferrari')
+--   AND sold IS FALSE;
+
+-- SELECT brand, model, color FROM cars
+--   WHERE color NOT IN ('red', 'blue', 'white')
+--   AND brand NOT IN ('Aston Martin', 'Bentley', 'Jaguar')
+--   AND sold IS FALSE;
+
+-- SELECT brand, model, year, sold FROM cars
+-- WHERE ((brand = 'Dodge' AND year between 1960 AND 1969)
+-- OR (brand IN ('Ford', 'Triumph') AND year BETWEEN 1970 AND 1979))
+-- AND sold IS FALSE;
+
+
+          -- SORT ORDER --
+-- SELECT brand, model, year FROM cars
+--   ORDER BY brand DESC, year;
+
+-- SELECT brand, model, condition, price FROM cars
+--   ORDER BY condition DESC, price;
+
+-- SELECT brand, model, condition, price FROM cars
+-- WHERE sold IS FALSE
+-- AND condition != 5
+-- ORDER BY condition DESC, price;
+
+-- SELECT brand, model, year, price FROM cars
+--   ORDER BY price DESC 
+--   LIMIT 1;
+
+-- SELECT brand, model, color, price FROM cars
+--   WHERE color LIKE '%red%'
+--   AND sold IS FALSE
+--   ORDER BY price 
+--   LIMIT 5;
+
+
+          -- AGGREGATES --
+-- SELECT COUNT(*) AS total_sold FROM cars
+--   WHERE sold IS TRUE;
+
+-- SELECT SUM(price) AS total_earnings FROM cars
+-- WHERE sold IS TRUE
+
+-- SELECT MAX(price) AS most_expensive FROM cars
+--   WHERE sold IS TRUE;
+
+-- SELECT AVG(price) FROM cars
+--   WHERE brand = 'Bentley';
+
+-- SELECT FLOOR(AVG(price)) AS average FROM cars
+--   WHERE brand = 'Bentley';
+
+-- SELECT CEIL(AVG(price)) AS avg, MAX(price), MIN(price) FROM cars
+--   WHERE sold IS TRUE;
+
+
+          -- GROUPING COLUMNS --
+--  SELECT brand, COUNT(brand) AS brand_count FROM cars
+--   GROUP BY brand;
+
+-- SELECT condition, COUNT(condition) FROM cars
+--   GROUP BY condition;
+
+-- SELECT brand, COUNT(brand), FLOOR(AVG(price)) as AVG 
+--   FROM cars
+--   WHERE sold IS FALSE
+--   GROUP BY brand;
+
+-- SELECT brand, COUNT(brand), FLOOR(AVG(price)) as AVG
+--   FROM cars
+--   WHERE sold IS FALSE
+--   GROUP BY brand
+--   HAVING COUNT(brand) > 1;
+
+-- SELECT year, COUNT(year) AS car_count, MAX(price), MIN(price)
+--   FROM cars
+--   WHERE sold IS TRUE
+--   GROUP BY year
+--   HAVING COUNT(year) > 1
+--   ORDER BY car_count;
+
+          -- CHALLENGES --
+-- SELECT brand, model, year
+--   FROM cars
+--   WHERE sold IS FALSE
+--   ORDER BY year
+--   LIMIT 5;
+
+-- SELECT color, COUNT(color)
+--   FROM cars
+--   WHERE sold IS NOT TRUE
+--   GROUP BY color
+--   HAVING COUNT(color) > 2
+--   ORDER BY COUNT(color) DESC;
+
+
+          -- MANIPULATING DATA --
+          --*(THESE DIDN'T REALLY SHOW IN DB, IT'S HARD CODED)* --
+-- INSERT INTO cars (
+--   brand, model, year, price, color, condition, sold
+-- ) VALUES (
+--   'Ford', 'Escort RS2000', 1978, 39000, 'blue', 5, FALSE
+-- ), (
+-- 	'Aston Martin', 'V8 Vantage', 1977, 145000, 'dark green', 5, FALSE
+-- ), (
+--   'Chevrolet', 'Bel Air', 1955, 50000, 'purple', 5, FALSE
+-- ), (
+--   'Porsche', '944 Turbo', 1986, 48000, 'white', 4, FALSE
+-- )
+
+-- UPDATE cars 
+--   SET sold = TRUE
+--   WHERE brand = 'FORD'
+--   AND model = 'Escort RS2000';
+
+-- UPDATE cars 
+--   SET condition = 5, price = 465000
+--   WHERE brand = 'Aston Martin'
+--   AND model = 'DB4'
+--   AND id = 14;
+
+-- UPDATE cars
+--   SET condition = 1, price = 10000
+--   WHERE brand = 'Porsche'
+--   AND sold IS FALSE;
+
+-- DELETE FROM cars WHERE condition = 0;
+
+-- DELETE FROM cars WHERE sold IS TRUE;
